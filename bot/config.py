ce5b5dbc22bd4df7d4bdb91a894a575aca70f692
Nirmalraj10567 <55886074+Nirmalraj10567@users.noreply.github.com>
2021-01-28 11:24:57 +0530

    Create config.py

---
+++ bot/config.py
@@ -0,0 +1,13 @@
+import config
+
+class config():
+
+    BOT_TOKEN = os.getenv("c")  # from @botfather
+
+    APP_ID = os.getenv("a")     # from https://my.telegram.org/apps
+
+    API_HASH = os.getenv("b")# from https://my.telegram.org/apps
+
+    API_KEY = os.getenv("k")# from https://mixdrop.co
+
+    API_EMAIL = os.getenv("e")  # from https://mixdrop.co
