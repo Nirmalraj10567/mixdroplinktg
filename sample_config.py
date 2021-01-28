3d2f08e6a9e08737d1f21d1891aa8c12c2389765
Nirmalraj10567 <55886074+Nirmalraj10567@users.noreply.github.com>
2021-01-28 10:52:34 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -1,6 +1,7 @@
+import config
 class config():
-    BOT_TOKEN = ''  # from @botfather
-    APP_ID = ''     # from https://my.telegram.org/apps
-    API_HASH = ''   # from https://my.telegram.org/apps
-    API_KEY = ''    # from https://mixdrop.co
-    API_EMAIL = ''  # from https://mixdrop.co
+    BOT_TOKEN = os.getenv("c")  # from @botfather
+    APP_ID = os.getenv("a")     # from https://my.telegram.org/apps
+    API_HASH = os.getenv("b")# from https://my.telegram.org/apps
+    API_KEY = os.getenv("k")# from https://mixdrop.co
+    API_EMAIL = os.getenv("e")  # from https://mixdrop.co
